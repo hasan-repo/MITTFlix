f5c97080164bd2ecab27bf10231d6aa5a8db1fb8
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 02:49:01 -0500

    add header, div and form in header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,31 @@ function Header(props){
 
     return (
         <header classname = "header">
-            
+          <a href="/"> 
+                <img 
+                src="https://fontmeme.com/permalink/190707/fd4735271a0d997cbe19a04408c896fc.png"
+                alt="netflix-font"
+                border="0"
+                />
+            </a>
+
+            <div id="navigation" className="navigation">
+                <nav>
+                <ul>
+                    <li><a href="/myList">My List</a></li>
+                </ul>
+                </nav>
+            </div>
+
+            <form id="search" className="search">
+                <input type="search" placeholder="Search for a title..." />
+                <div className="searchResults"></div>
+            </form>
+
 
         </header>
 
     )
 }
+
+export default Header
