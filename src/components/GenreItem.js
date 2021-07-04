0a7d3442a56cec6f66a4c86ed7b23e880ae4e78f
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 16:34:30 -0500

    add movie count and fix genre name

--- src/components/GenreItem.js
+++ src/components/GenreItem.js
@@ -2,13 +2,23 @@ import React from 'react'
 import MovieItem from "./MovieItem"
 
 function GenreItem(props) {
+    const { genreItem } = props
+
+    //if movir count 0 retun empty tag
+    if (genreItem.movies.length === 0) { 
+        return (<></>)
+    }
 
     return(
         <div className="titleList">
             <div className="title">
-            <h1>Action</h1>
+            <h1>{ genreItem.name }</h1>
                 <div className="titles-wrapper">
-                    <MovieItem />
+                    {
+                         genreItem.movies.map((movie, index) => (
+                            <MovieItem movieItem={ movie } key={ index } /> 
+                        ))
+                    }
                 </div> 
             </div>
         </div>
