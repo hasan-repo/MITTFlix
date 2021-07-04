04f90bacd020b8ee3392f902c316a78d2c93ca6d
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 16:52:07 -0500

    update

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,26 +1,20 @@
 import React from 'react';
+const {movieItem} = props;
+
 
 function MovieItem(props){
 
     return(
         <div className = "movie">
             <img
-                src="https://image.tmdb.org/t/p/w500/fddtVEUozxvLmehR5yGZjem44pD.jpg"
+                src = { movieItem.poster_path }
             />
 
             <div className="overlay">
-                <div className="title">Ant-Man</div>
+                <div className="title">{ movieItem.title }</div>
                 <div className="rating">7.1/10</div>
-                <div className="plot">
-                Armed with the astonishing ability to shrink in scale but
-                increase in strength, master thief Scott Lang must embrace his
-                inner-hero and help his mentor, Doctor Hank Pym, protect the
-                secret behind his spectacular Ant-Man suit from a new
-                generation of towering threats. Against seemingly
-                insurmountable obstacles, Pym and Lang must plan and pull off
-                a heist that will save the world.
-                </div>
-                <div data-toggled="true" className="listToggle">
+                <div className="plot">{ movieItem.overview }</div>
+                <div data-toggled = { movieItem.my_list } className="listToggle">
                 <div>
                     <i className="fa fa-fw fa-plus"></i
                     ><i className="fa fa-fw fa-check"></i>
