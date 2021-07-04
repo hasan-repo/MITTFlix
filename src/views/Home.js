44dc8800a5281c53cee834ec818b71876de843b0
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 17:31:51 -0500

    add useeffect and get movie data

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+
 import GenreItem from "../components/GenreItem"
+import * as MovieAPI from '../MovieAPI'
 
 function Home(props){
+
+    // api context
+    
+    useEffect(() => {
+        async function fetchAllMovies() {
+            const movies = await MovieAPI.getAll()
+            return movies
+        }
+
+        async function fetchGenres() {
+           const all_movies = await fetchAllMovies()
+           let genre = await MovieAPI.genres()
+        }
+        
+        fetchGenres()
+        
+    }, [])
 
     return(
         <>
