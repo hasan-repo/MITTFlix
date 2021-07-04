6e19ec87a58faba7b7490fcce5917c1cdf29cbfb
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 17:53:11 -0500

    complete async function

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import GenreItem from "../components/GenreItem"
 import * as MovieAPI from '../MovieAPI'
 
-function Home(props){
 
-    // api context
+function Home(props) {
+
+    const [ moviesData, setMoviesData ] = useState([])
     
     useEffect(() => {
         async function fetchAllMovies() {
@@ -14,19 +15,29 @@ function Home(props){
         }
 
         async function fetchGenres() {
-           const all_movies = await fetchAllMovies()
-           let genre = await MovieAPI.genres()
+            const all_movies = await fetchAllMovies()
+            let genre = await MovieAPI.genres()
+            for(let i = 0; i < genre.length; i ++) {
+                genre[i]['movies'] = all_movies.filter(item => item.genre_ids.includes(genre[i]['id']) === true)
+            }
+            console.log(genre)
+            setMoviesData(genre)
         }
         
         fetchGenres()
         
     }, [])
 
-    return(
+    return (
         <>
-        <GenreItem />
-    </>
+            {
+                moviesData.map((moviesCell, index) => (
+                    <GenreItem genreItem={ moviesCell } key={ index } />
+                ))
+            }
+        </>
+
     )
 }
 
-export default Home 
+export default Home
