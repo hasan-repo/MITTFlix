705d579387ab4a30485cb57dc9c881ac4d39a434
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 13:04:27 -0500

    add eventlistener onclick event to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header(props) {
 
     return (
         <header className ="header">
-            <a href="/">
+            <a onClick={ (e) => { e.preventDefault(); props.history.push('/')}}>
                 <img
                 src="https://fontmeme.com/permalink/190707/fd4735271a0d997cbe19a04408c896fc.png"
                 alt="netflix-font"
@@ -15,7 +15,7 @@ function Header(props) {
             <div id="navigation" className="navigation">
                 <nav>
                 <ul>
-                    <li><a href="/myList">My List</a></li>
+                    <li><a onClick={ (e) => { e.preventDefault(); props.history.push('/my-list')}}>My List</a></li>
                 </ul>
                 </nav>
             </div>
