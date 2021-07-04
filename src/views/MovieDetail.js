ab658ee3e86075e05687952444902c0a8f6c3799
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 12:31:26 -0500

    Create mylist.js in views

--- src/views/MovieDetail.js
+++ src/views/MovieDetail.js
@@ -1,2 +1,10 @@
 import React from 'react'
 
+function MovieDetail (props) {
+
+    return(
+        <div>movie detail page</div>
+    )
+}
+
+export default MovieDetail 
---
+++ src/views/MyList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function MyList(props){
+
+    return(
+        <div>my list page</div>
+    )
+}
+
+export default MyList
