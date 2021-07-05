59680080805796629b0f074255b3aad5742c85bb
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-05 02:33:29 -0500

    create get result function

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import MovieItem from "../components/MovieItem"
+import * as MovieAPI from '../MovieAPI'
 
-function Search () {
+function Search (props) {
+ // get movies use api and set key
+//   count search result.
 
+    async function getSearchResult() {
+        let result = []
+        if (page === '') {
+            result = await MovieAPI.getSearchResult(`movies?q=${ key }`)
+        }
+        else {
+            result = await MovieAPI.getSearchResult(`movies?my_list=true&q=${ key }`)
+            
+        }
 
 }
 
