23ad9e4cd28c2fc534fa6d762d3ff6d94982d92f
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 22:32:02 -0500

    delete async function from home and update

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,32 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 
 import GenreItem from "../components/GenreItem"
-import * as MovieAPI from '../MovieAPI'
+import { AppContext } from "../App"
 
 
-function Home(props) {
+function Home() {
 
-    const [ moviesData, setMoviesData ] = useState([])
+    const { moviesData } = useContext(AppContext)
     
-    useEffect(() => {
-        async function fetchAllMovies() {
-            const movies = await MovieAPI.getAll()
-            return movies
-        }
+    // useEffect(() => {
+    //     async function fetchAllMovies() {
+    //         const movies = await MovieAPI.getAll()
+    //         return movies
+    //     }
 
-        async function fetchGenres() {
-            const all_movies = await fetchAllMovies()
-            let genre = await MovieAPI.genres()
-            for(let i = 0; i < genre.length; i ++) {
-                genre[i]['movies'] = all_movies.filter(item => item.genre_ids.includes(genre[i]['id']) === true)
-            }
-            console.log(genre)
-            setMoviesData(genre)
-        }
+    //     async function fetchGenres() {
+    //         const all_movies = await fetchAllMovies()
+    //         let genre = await MovieAPI.genres()
+    //         for(let i = 0; i < genre.length; i ++) {
+    //             genre[i]['movies'] = all_movies.filter(item => item.genre_ids.includes(genre[i]['id']) === true)
+    //         }
+    //         console.log(genre)
+    //         setMoviesData(genre)
+    //     }
         
-        fetchGenres()
+    //     fetchGenres()
         
-    }, [])
+    // }, [])
 
     return (
         <>
