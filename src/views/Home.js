2a49758a5ace9e40546591aa854fdc6a99f6301b
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-05 00:43:09 -0500

    update Mylist.js and get movie list

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,25 +8,6 @@ function Home() {
 
     const { moviesData } = useContext(AppContext)
     
-    // useEffect(() => {
-    //     async function fetchAllMovies() {
-    //         const movies = await MovieAPI.getAll()
-    //         return movies
-    //     }
-
-    //     async function fetchGenres() {
-    //         const all_movies = await fetchAllMovies()
-    //         let genre = await MovieAPI.genres()
-    //         for(let i = 0; i < genre.length; i ++) {
-    //             genre[i]['movies'] = all_movies.filter(item => item.genre_ids.includes(genre[i]['id']) === true)
-    //         }
-    //         console.log(genre)
-    //         setMoviesData(genre)
-    //     }
-        
-    //     fetchGenres()
-        
-    // }, [])
 
     return (
         <>
