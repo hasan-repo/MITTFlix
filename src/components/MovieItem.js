198c248754aad25958ae0bac8b1d2d17b7ea7426
MD Anamul Hasan <78699472+hasan-repo@users.noreply.github.com>
2021-07-04 22:16:47 -0500

    update

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,31 +1,40 @@
-import React from 'react';
+import React, { useState, useContext } from 'react'
 
+import { AppContext } from "../App"
 
+import * as MovieAPI from '../MovieAPI'
 
-function MovieItem(props){
+function MovieItem(props) {
 
-    const {movieItem} = props
+    const { movieItem } = props
 
-    return(
-        <div className = "movie">
+    
+
+    const { handleToggle } = useContext(AppContext)
+    const handleToggleBtn = () => {
+        
+        handleToggle(movieItem)
+    }
+
+    return (
+        <div className="movie">
             <img
-                src = { movieItem.poster_path }
+                src={ movieItem.poster_path } 
             />
-
             <div className="overlay">
                 <div className="title">{ movieItem.title }</div>
-                <div className="rating">{movieItem.vote_average}/10</div>
-                <div className="plot">{ movieItem.overview }</div>
-                <div data-toggled = { movieItem.my_list } className="listToggle">
-                <div>
-                    <i className="fa fa-fw fa-plus"></i
-                    ><i className="fa fa-fw fa-check"></i>
+                <div className="rating">7.1/10</div>
+                <div className="plot">
+                    { movieItem.overview }
                 </div>
+                <div data-toggled={ movieItem.my_list } className="listToggle" onClick={ () => handleToggleBtn() }>
+                    <div>
+                        <i className="fa fa-fw fa-plus"></i
+                        ><i className="fa fa-fw fa-check"></i>
+                    </div>
                 </div>
             </div>
-            
         </div>
-
     )
 }
 
